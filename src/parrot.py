df4ced295a71a949459cf1eb56f41a22b63912d7
Juan M. Estévez Ferro <jeferro@gmail.com>
2025-03-07 21:14:28 +0100

    Create a factory method to create parrots

--- src/parrot.py
+++ src/parrot.py
@@ -9,22 +9,33 @@ class ParrotType(Enum):
 
 class Parrot:
 
-    def __init__(self, type_of_parrot, number_of_coconuts, voltage, nailed):
+    def __init__(self,
+                 type_of_parrot: ParrotType.EUROPEAN,
+                 number_of_coconuts: float,
+                 voltage: float,
+                 nailed: bool):
         self._type = type_of_parrot
         self._number_of_coconuts = number_of_coconuts
         self._voltage = voltage
         self._nailed = nailed
 
-    def speed(self):
+    @staticmethod
+    def create_parrot(type_of_parrot: ParrotType.EUROPEAN,
+                 number_of_coconuts: float,
+                 voltage: float,
+                 nailed: bool):
+        return Parrot(type_of_parrot, number_of_coconuts, voltage, nailed)
+
+    def speed(self) -> float:
         match self._type:
             case ParrotType.EUROPEAN:
                 return self._base_speed()
             case ParrotType.AFRICAN:
-                return max(0, self._base_speed() - self._load_factor() * self._number_of_coconuts)
+                return max(0.0, self._base_speed() - self._load_factor() * self._number_of_coconuts)
             case ParrotType.NORWEGIAN_BLUE:
                 return 0 if self._nailed else self._compute_base_speed_for_voltage(self._voltage)
 
-    def cry(self):
+    def cry(self) -> str:
         match self._type:
             case ParrotType.EUROPEAN:
                 return "Sqoork!"
@@ -36,8 +47,10 @@ class Parrot:
     def _compute_base_speed_for_voltage(self, voltage):
         return min([24.0, voltage * self._base_speed()])
 
-    def _load_factor(self):
-        return 9.0
-
-    def _base_speed(self):
+    @staticmethod
+    def _base_speed():
         return 12.0
+
+    @staticmethod
+    def _load_factor():
+        return 9.0
