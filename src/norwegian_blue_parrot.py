e241c3a23570c45141fa92e27cc5cf5182bb858c
Juan M. Estévez Ferro <jeferro@gmail.com>
2025-03-07 22:04:19 +0100

    Create constant MIN_SPEED

--- src/norwegian_blue_parrot.py
+++ src/norwegian_blue_parrot.py
@@ -1,5 +1,7 @@
 from src.parrot import Parrot
 
+MIN_SPEED = 24.0
+
 
 class NorwegianBlueParrot(Parrot):
 
@@ -17,7 +19,7 @@ class NorwegianBlueParrot(Parrot):
         return 0 if self._nailed else self._compute_base_speed_for_voltage(self._voltage)
 
     def _compute_base_speed_for_voltage(self, voltage):
-        return min([24.0, voltage * self._base_speed()])
+        return min([MIN_SPEED, voltage * self._base_speed()])
 
     def cry(self) -> str:
         return "Bzzzzzz" if self._voltage > 0 else "..."
