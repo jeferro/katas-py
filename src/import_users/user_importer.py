ee9d7df2457cb0c76644718fe10d9e145b89e9df
Juan M. Estévez Ferro <jeferro@gmail.com>
2025-12-17 14:43:38 +0100

    refactor: split code in two methods: fetch_from_web and fetch_from_csv

--- src/import_users/user_importer.py
+++ src/import_users/user_importer.py
@@ -1,28 +1,24 @@
-import os
 import csv
-import json
-import urllib.request
 import datetime
+import json
+import os
+import urllib.request
 
 
 class UserImporter:
 
     @staticmethod
     def import_users():
+        csv_provider = UserImporter.fetch_from_csv()
+
+        b = UserImporter.fetch_from_web()
+
+        # merge arrays
+        return csv_provider + b
+
+    @staticmethod
+    def fetch_from_web() -> list[list[str]]:
         USER_URL = 'https://randomuser.me/api/?inc=gender,name,email,location&results=5&seed=a9b25cd955e2037h'
-
-        # Parse CSV file
-        getcurrentworkingDirectory = os.getcwd()
-
-        # fields: ID, gender, Name ,country, postcode, email, Birthdate
-        with open(os.path.join(getcurrentworkingDirectory, 'users.csv'), 'r') as f:
-            reader = csv.reader(f)
-            csv_provider = list(reader)
-
-        csvProviders = []
-        for a in csv_provider:
-            csvProviders.append(csv_provider[0] + a)
-        csv_provider.pop(0)  # Remove header column
 
         # Parse URL content
         url = USER_URL
@@ -47,6 +43,20 @@ class UserImporter:
                     item['email'],
                     datetime.datetime.now().strftime('%Y')  # birthday
                 ])
+        return b
 
-        # merge arrays
-        return csv_provider + b
+    @staticmethod
+    def fetch_from_csv() -> list[list[str]]:
+        # Parse CSV file
+        getcurrentworkingDirectory = os.getcwd()
+
+        # fields: ID, gender, Name ,country, postcode, email, Birthdate
+        with open(os.path.join(getcurrentworkingDirectory, 'users.csv'), 'r') as f:
+            reader = csv.reader(f)
+            csv_provider = list(reader)
+
+        csvProviders = []
+        for a in csv_provider:
+            csvProviders.append(csv_provider[0] + a)
+        csv_provider.pop(0)  # Remove header column
+        return csv_provider
