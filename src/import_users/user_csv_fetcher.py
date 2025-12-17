370b93c6164610d13de761dc8a2a4b372465a028
Juan M. Estévez Ferro <jeferro@gmail.com>
2025-12-17 15:08:08 +0100

    refactor: rename variables in user_csv_fetcher.py

--- src/import_users/user_csv_fetcher.py
+++ src/import_users/user_csv_fetcher.py
@@ -8,16 +8,18 @@ class UserCsvFetcher(UserFetcher):
 
     def fetch(self) -> list[list[str]]:
         # Parse CSV file
-        getcurrentworkingDirectory = os.getcwd()
+        current_working_directory = os.getcwd()
+        csv_path = os.path.join(current_working_directory, 'users.csv')
 
         # fields: ID, gender, Name ,country, postcode, email, Birthdate
-        with open(os.path.join(getcurrentworkingDirectory, 'users.csv'), 'r') as f:
+        with open(csv_path, 'r') as f:
             reader = csv.reader(f)
             csv_provider = list(reader)
 
         csvProviders = []
         for a in csv_provider:
             csvProviders.append(csv_provider[0] + a)
+
         csv_provider.pop(0)  # Remove header column
 
         return csv_provider
