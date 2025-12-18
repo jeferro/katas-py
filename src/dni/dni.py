43470d38258c98fbeddc38509151101d6a518b8c
Juan M. Estévez Ferro <jeferro@gmail.com>
2025-12-18 11:17:45 +0100

    feature: add nie support

--- src/dni/dni.py
+++ src/dni/dni.py
@@ -4,46 +4,64 @@ from src.dni.validation_error import ValidationError
 
 
 class DNI(object):
-    _LETTERS = ["T",
-                "R",
-                "W",
-                "A",
-                "G",
-                "M",
-                "Y",
-                "F",
-                "P",
-                "D",
-                "X",
-                "B",
-                "N",
-                "J",
-                "Z",
-                "S",
-                "Q",
-                "V",
-                "H",
-                "L",
-                "C",
-                "K",
-                "E"]
+    _NIE_CHARACTERS = [
+        "X",
+        "Y",
+        "Z",
+    ]
+
+    _CONTROL_DIGITS = [
+        "T",
+        "R",
+        "W",
+        "A",
+        "G",
+        "M",
+        "Y",
+        "F",
+        "P",
+        "D",
+        "X",
+        "B",
+        "N",
+        "J",
+        "Z",
+        "S",
+        "Q",
+        "V",
+        "H",
+        "L",
+        "C",
+        "K",
+        "E"]
 
     def __init__(self,
+                 letter: str,
                  numbers: List[int],
                  control_digit: str):
+        self._letter = letter
         self._numbers = numbers
         self._control_digit = control_digit
 
     @staticmethod
-    def create_of_value(value: str):
+    def create(value: str):
         if len(value) != 9:
             raise ValidationError(f"DNI length must be 9: {value}")
 
+        letter = None
         numbers = []
         control_digit = None
 
         for index, character in enumerate(value):
-            if index < 8:
+            if index == 0:
+                if character.isnumeric():
+                    number = int(character)
+
+                    numbers.append(number)
+                else:
+                    letter = character
+
+            elif index < 8:
                 if not character.isnumeric():
                     raise ValidationError(f"DNI character in index {index} must be a numeric: {value}")
 
@@ -56,16 +74,19 @@ class DNI(object):
                 if not last_character:
                     raise ValidationError(f"DNI last character must be a letter: {value}")
 
-                control_digit = DNI._calculate_control_digit(numbers)
+                control_digit = DNI._calculate_control_digit(letter, numbers)
 
                 if last_character != control_digit:
                     raise ValidationError(f"DNI control digit invalid: {value}. Expected digit control: {control_digit}")
 
 
-        return DNI(numbers, control_digit)
+        return DNI(letter, numbers, control_digit)
 
     def value(self) -> str:
         output = ""
+
+        if self._letter is not None:
+            output += self._letter
 
         for number in self._numbers:
             output += str(number)
@@ -75,9 +96,12 @@ class DNI(object):
         return output
 
     @staticmethod
-    def _calculate_control_digit(numbers: List[int]) -> str:
+    def _calculate_control_digit(letter, numbers: List[int]) -> str:
         sum = 0
-        total_letters = len(DNI._LETTERS)
+        total_letters = len(DNI._CONTROL_DIGITS)
+
+        if letter is not None:
+            sum += DNI._NIE_CHARACTERS.index(letter)
 
         for number in numbers:
             sum += number
@@ -87,4 +111,4 @@ class DNI(object):
         if index >= total_letters:
             raise ValidationError(f"Out of index: {index}")
 
-        return DNI._LETTERS[index]
+        return DNI._CONTROL_DIGITS[index]
--- src/dni/dni_test.py
+++ src/dni/dni_test.py
@@ -4,34 +4,41 @@ from src.dni.dni import DNI
 from src.dni.validation_error import ValidationError
 
 
-def test_valid_dni():
+def test_valid_nif():
     raw_dni = "31970165P"
 
-    dni = DNI.create_of_value(value=raw_dni)
+    dni = DNI.create(value=raw_dni)
+
+    assert dni.value() == raw_dni
+
+def test_valid_nie():
+    raw_dni = "Z5090857N"
+
+    dni = DNI.create(value=raw_dni)
 
     assert dni.value() == raw_dni
 
 
 def test_error_dni_length_less_than_9():
     with pytest.raises(ValidationError):
-        DNI.create_of_value(value="3197016P")
+        DNI.create(value="3197016P")
 
 
 def test_error_dni_length_greater_than_9():
     with pytest.raises(ValidationError):
-        DNI.create_of_value(value="319701655P")
+        DNI.create(value="319701655P")
 
 
 def test_first_8_characters_should_be_numbers():
     with pytest.raises(ValidationError):
-        DNI.create_of_value(value="3A970165P")
+        DNI.create(value="3A970165P")
 
 
 def test_last_characters_should_be_a_letter():
     with pytest.raises(ValidationError):
-        DNI.create_of_value(value="319701657")
+        DNI.create(value="319701657")
 
 
 def test_letter_validation():
     with pytest.raises(ValidationError):
-        DNI.create_of_value(value="31970165A")
+        DNI.create(value="31970165A")
